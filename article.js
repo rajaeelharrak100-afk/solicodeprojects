8b8b64cde768c09f33860aaaada3748fbb430fe6
rajaeelharrak100-afk <rajaeelharrak100@gmail.com>
2026-09-10 09:57:43 +0100

    Add files via upload

---
+++ article.js
@@ -0,0 +1,10 @@
+let prix = 60;
+let quantite = 3;
+let produit = prix * quantite;
+let somme = prix + quantite;
+let difference = prix - quantite;
+let quotient = prix / quantite
+console.log("produit", produit);
+console.log("somme", somme);
+console.log("difference", difference);
+console.log("quotient", quotient);
